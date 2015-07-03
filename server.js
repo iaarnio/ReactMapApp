a6f52f6dcddab8cc0ac80486fc7bb3c9bb3fecf6
Ilari Aarnio <jia@iki.fi>
2015-07-03 15:41:12 +0300

    rearrange folder structure

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 var express = require('express');
 var app = express();
 
-app.use(express.static(__dirname));
+app.use(express.static(__dirname + '/app'));
 
 app.listen(3000, function() {
   console.log('Listening on port 3000...')
