15a60917fdb7796d946d2a72b573f454ead88762
Ilari Aarnio <jia@iki.fi>
2015-12-05 15:10:16 +0200

    add openshift settings

--- server.js
+++ server.js
@@ -3,6 +3,9 @@ var app = express();
 
 app.use(express.static(__dirname + '/app'));
 
-app.listen(3000, function() {
-  console.log('Listening on port 3000...')
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
+
+app.listen(server_port, server_ip_address, function() {
+  console.log('Listening on ' + server_ip_address + ', server_port ' + port)
 })
