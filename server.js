3cd60a987b6060dc7b2d805e84ca61f04ac161c4
Ilari Aarnio <ilari.aarnio@futurice.com>
2018-04-22 13:01:30 +0300

    Fix typo

--- server.js
+++ server.js
@@ -3,8 +3,8 @@ var app = express();
 
 app.use(express.static(__dirname + '/app'));
 
-var server_port = 80
-var server_ip_address = 127.0.0.1
+var server_port = 80;
+var server_ip_address = '127.0.0.1';
 
 app.listen(server_port, server_ip_address, function() {
   console.log('Listening on ' + server_ip_address + ', server_port ' + server_port)
