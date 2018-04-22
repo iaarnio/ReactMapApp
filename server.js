4e47e07835d8d79194ac2b09abbae99bb1e25d2b
Ilari Aarnio <ilari.aarnio@futurice.com>
2018-04-22 13:24:30 +0300

    Change port

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var app = express();
 
 app.use(express.static(__dirname + '/app'));
 
-var server_port = 8080;
+var server_port = 80;
 var server_ip_address = 'hungry-boyd-2f1227.netlify.com';
 
 app.listen(server_port, server_ip_address, function() {
